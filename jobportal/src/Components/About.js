c4a23fbe78202497e8fb4294120291cb516a35dc
Sathees Kumar K <65384711+sathees-saty@users.noreply.github.com>
2024-04-06 10:41:12 +0530

    Add files via upload

---
+++ jobportal/src/Components/About.js
@@ -0,0 +1,12 @@
+// About.js 
+
+import React from 'react'
+
+export const About = () => { 
+return ( 
+	<div className='text-center qform'> 
+		<h3>This is About Section</h3> 
+		<p>This is a Job Portal website example using ReactJS</p> 
+	</div> 
+) 
+}
